399039facea0743e8ad5f4607927a41fcbdb20dc
Matthieu Mota <matthieumota@gmail.com>
2022-02-09 14:19:04 +0100

    Upload

--- 11-fichiers/index.php
+++ 11-fichiers/index.php
@@ -1,24 +1,36 @@
 <?php
 $photo = $_FILES['photo'] ?? null;
 $errors = [];
+$success = null;
 
 var_dump($photo);
 
 if (!empty($_FILES)) {
     // Vérifier les erreurs du fichier (taille, type)
-
-    // On va s'assurer que le dossier uploads existe sur le serveur
-    if (!is_dir('uploads')) {
-        mkdir('uploads');
+    $mime = mime_content_type($photo['tmp_name']); // image/jpeg
+    $mimeTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'];
+    // On vérifie que le fichier uploadé est bien une image...
+    if (!in_array($mime, $mimeTypes)) {
+        $errors[] = 'Le fichier doit être une image';
     }
 
-    $filename = $photo['name']; // avatar.jpeg
-    // avatar.jpeg devient avatar-123456.jpeg
-    $file = pathinfo($filename); // => ['filename' => 'avatar', 'extension' => 'jpeg'];
-    $filename = $file['filename'].'-'.uniqid().'.'.$file['extension']; // avatar-123456.jpeg
-    // On upload le fichier sur le serveur
-    move_uploaded_file($photo['tmp_name'], 'uploads/'.$filename);
-}
+    // On fait l'upload s'il n'y a pas d'erreurs
+    if (empty($errors)) {
+        // On va s'assurer que le dossier uploads existe sur le serveur
+        if (!is_dir('uploads')) {
+            mkdir('uploads');
+        }
+
+        $filename = $photo['name']; // avatar.jpeg
+        // avatar.jpeg devient avatar-123456.jpeg
+        $file = pathinfo($filename); // => ['filename' => 'avatar', 'extension' => 'jpeg'];
+        $filename = $file['filename'].'-'.uniqid().'.'.$file['extension']; // avatar-123456.jpeg
+        // On upload le fichier sur le serveur
+        move_uploaded_file($photo['tmp_name'], 'uploads/'.$filename);
+
+        $success = 'uploads/'.$filename;
+    } // fin du empty($errors)
+} // fin du empty($_FILES)
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -33,5 +45,10 @@ if (!empty($_FILES)) {
 
         <button>Upload</button>
     </form>
+
+    <?php if ($success) { ?>
+        <p>Voici la photo que vous avez envoyé.</p>
+        <img src="<?php echo $success; ?>" alt="">
+    <?php } ?>
 </body>
 </html>
