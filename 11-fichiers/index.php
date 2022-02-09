e26fe9eb7c16414a31c30f3229cacfe8408c13f0
Matthieu Mota <matthieumota@gmail.com>
2022-02-09 15:01:13 +0100

    Upload check errors

--- 11-fichiers/index.php
+++ 11-fichiers/index.php
@@ -7,6 +7,12 @@ var_dump($photo);
 
 if (!empty($_FILES)) {
     // Vérifier les erreurs du fichier (taille, type)
+    // 4096 = 4ko
+    // 4096 * 1024 = 4Mo
+    if ($photo['size'] > 4096) {
+        $errors[] = "L'image doit faire 4ko maximum.";
+    }
+
     $mime = mime_content_type($photo['tmp_name']); // image/jpeg
     $mimeTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'];
     // On vérifie que le fichier uploadé est bien une image...
@@ -40,11 +46,25 @@ if (!empty($_FILES)) {
     <title>Upload</title>
 </head>
 <body>
+    <ul>
+        <?php foreach ($errors as $error) { ?>
+            <li><?php echo $error; ?></li>
+        <?php } ?>
+    </ul>
+
     <form method="post" enctype="multipart/form-data">
         <input type="file" name="photo">
 
         <button>Upload</button>
     </form>
+
+    <?php
+        // Renvoie tous les fichiers du dossier uploads dans un tableau
+        $photos = glob('uploads/*');
+
+        foreach ($photos as $photo) { ?>
+            <img width="80" src="<?php echo $photo; ?>" alt="">
+    <?php } ?>
 
     <?php if ($success) { ?>
         <p>Voici la photo que vous avez envoyé.</p>
